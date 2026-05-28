20142593e5f398e4cb01427b1cffee2316706646
alkimcogneuro <albert.kim@colorado.edu>
2026-05-28 13:27:34 -0600

    Update plot_allchans_CSD_byfreqs.m

--- plot_allchans_CSD_byfreqs.m
+++ plot_allchans_CSD_byfreqs.m
@@ -28,22 +28,35 @@ function [] = plot_allchans_CSD_byfreqs(csd_results)
     csd_upperbd_freq = 8;  % upper bound of frequency range of interest
 
     idx = (csd_results.Freqs >= csd_lowerbd_freq) & (csd_results.Freqs <= csd_upperbd_freq);  % logical index for frequencies between 2 and 5 Hz
+
     csd_avg_2_5Hz_by_channel = mean(abs(csd_results.CSD_chanmeans(:, idx)), 2);  % average across frequencies (dimension 2), resulting in a vector of length num_channels.
     % we need to convert to double precision for plotting, 
     % I think this is because the plotting code assumes that the input values can be very small, 
     % and when we plot them, they will be rounded to zero if we keep them in single precision.
-
     csd_avg_2_5Hz_by_channel = double(csd_avg_2_5Hz_by_channel);  % convert to double precision for plotting
     fprintf('csd_avg 2-5 Hz \n');
-    class(csd_avg_2_5Hz_by_channel)  
-    disp(csd_avg_2_5Hz_by_channel);
-
     channel_locations = csd_results.Chanlocs; 
     csd_avg_2_5Hz_by_channel = csd_avg_2_5Hz_by_channel(:);   % this conversion might not be necessary, but it ensures that the input to plot_topomap is a column vector, which is what the function expects.
     %% plot_topomap(csd_avg_2_5Hz_by_channel', csd_results.Chanlocs)  % plot the topomap using the random voltages and the channel locations from the EEG data file.
-    call_topoplot_eeglab(csd_avg_2_5Hz_by_channel', csd_results.Chanlocs)  % plot the topomap using the random voltages and the channel locations from the EEG data file.
+    call_topoplot_eeglab(csd_avg_2_5Hz_by_channel', csd_results.Chanlocs,'CSD', 'CSD')  % plot the topomap using the random voltages and the channel locations from the EEG data file.
     for idx = 1:length(csd_results.Chanlocs)
         fprintf('channel %s, CSD(2-5Hz) = %d\n',  csd_results.Chanlocs(idx).labels,  csd_avg_2_5Hz_by_channel(idx) );
+    end
+
+    % plot the MSC values (which are the coherence values) for each channel, averaged across frequencies between 2 and 5 Hz, in a topographic map.
+    % these are based on CSD averaged across trials.
+    msc_avg_2_5Hz_by_channel = mean(abs(csd_results.MSC_chanmeans(:, idx)), 2);  % average across frequencies (dimension 2), resulting in a vector of length num_channels.
+    % we need to convert to double precision for plotting, 
+    % I think this is because the plotting code assumes that the input values can be very small, 
+    % and when we plot them, they will be rounded to zero if we keep them in single precision.
+    msc_avg_2_5Hz_by_channel = double(msc_avg_2_5Hz_by_channel);  % convert to double precision for plotting
+    fprintf('csd_avg 2-5 Hz \n');
+    channel_locations = csd_results.Chanlocs; 
+    msc_avg_2_5Hz_by_channel = msc_avg_2_5Hz_by_channel(:);   % this conversion might not be necessary, but it ensures that the input to plot_topomap is a column vector, which is what the function expects.
+
+    call_topoplot_eeglab(msc_avg_2_5Hz_by_channel', csd_results.Chanlocs,'MSC', 'MSC')  % plot the topomap using the random voltages and the channel locations from the EEG data file.
+    for idx = 1:length(csd_results.Chanlocs)
+        fprintf('channel %s, MSC(2-5Hz) = %d\n',  csd_results.Chanlocs(idx).labels,  msc_avg_2_5Hz_by_channel(idx) );
     end
 
     figure; 
@@ -52,11 +65,9 @@ function [] = plot_allchans_CSD_byfreqs(csd_results)
         %%Results_CSD_Analysis.Chanlocs.labels
         csd_channel_means = csd_results.CSD_chanmeans(channel, :);
         csd_channel_mags = abs(csd_channel_means);
-        
         freqs = csd_results.Freqs;
         % Identify the indices of frequencies between 2 and 20 Hz, 
         % so that we can plot the CSD values in that range.
-        
         idx = (freqs >= 0.5) & (freqs <= 20);
         subplot(ceil(length(csd_results.Chanlocs)/4), 4, channel);
         % grab the magnitude of the CSD values for the specified channel, and plot them against the frequencies:
@@ -64,6 +75,24 @@ function [] = plot_allchans_CSD_byfreqs(csd_results)
         xlabel('Frequency (Hz)');
         ylabel('Cross-Spectral Density');
         title(sprintf('Cross-Spectral Density for Channel %d %s', channel, chanlabel));
-
     end
+
+    % figure; 
+    % for channel = 1:length(csd_results.Chanlocs) 
+    %     chanlabel = csd_results.Chanlocs(channel).labels;
+    %     %%Results_CSD_Analysis.Chanlocs.labels
+    %     msc_channel_means = csd_results.MSC_chanmeans(channel, :);
+        
+    %     freqs = csd_results.Freqs;
+    %     % Identify the indices of frequencies between 2 and 20 Hz, 
+    %     % so that we can plot the CSD values in that range.
+    %     idx = (freqs >= 0.5) & (freqs <= 20);
+    %     subplot(ceil(length(csd_results.Chanlocs)/4), 4, channel);
+    %     % grab the magnitude of the CSD values for the specified channel, and plot them against the frequencies:
+    %     plot(freqs(idx), msc_channel_means(idx), 'LineWidth', 2); 
+    %     xlabel('Frequency (Hz)');
+    %     ylabel('Magnitude Squared Coherence');
+    %     title(sprintf('MSC for Channel %d %s', channel, chanlabel));
+    % end
+
 end
