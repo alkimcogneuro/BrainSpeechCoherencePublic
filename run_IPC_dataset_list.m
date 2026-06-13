d377841c2880e55e2649a7119b4f8e1d52cfcfed
alkimcogneuro <albert.kim@colorado.edu>
2026-06-13 11:56:26 -0600

    Update run_IPC_dataset_list.m

--- run_IPC_dataset_list.m
+++ run_IPC_dataset_list.m
@@ -1,32 +1,20 @@
 
 function [] = run_IPC_dataset_list(eeg_data_filenames, speech_files_path, highpass_cutoff, lowpass_cutoff, options)
-    % input arguments:
-    %   eeg_data_filenames: cell array of strings; each string is a filepath to an EEG data file.
-    %                       I'm assuming that each EEG data file contains data from one subject and one condition, 
-    %                       and that the filename includes the subject ID and condition (e.g., "SE0001_P1_eeg_data.mat").
-    %   speech_files_path: string, path to the folder containing the speech .wav files.  
-    %                      The names of the individual .wav files should match the audio_file field in the EEG data structures.
-    %   highpass_cutoff: scalar, high-pass filter cutoff frequency in Hz (e.g., 2 Hz)
-    %   lowpass_cutoff: scalar, low-pass filter cutoff frequency in Hz (e.g., 35 Hz)
-    % how to call with optional arguments;
+    % This script runs the Instantaneous Phase Coherence analysis analysis for all data files in a list of filenames, using the specified filter parameters.
+    % For each data file, it runs Apply2Dataset_InstaPhaseCoherence() to analyze the entrainment between the EEG data and the corresponding speech data,    
     % 
     arguments
-        eeg_data_filenames (1,:) cell {mustBeText}  % cell array of strings; each string is a filepath to an EEG data file.
+        eeg_data_filenames (1,:) cell {mustBeText}   % cell array of strings; each string is a filepath to an EEG data file.
         speech_files_path (1,1) string {mustBeText}  % string, path to the folder containing the speech .wav files.  
-                                     % The names of the individual .wav files should match the audio_file field in the EEG data structures.
-        highpass_cutoff (1,1) double {mustBeReal} = 2  % highpass_cutoff defaults to 2 Hz 
-        lowpass_cutoff  (1,1) double {mustBeReal} = 35  % lowpass_cutoff defaults to 35 Hz
+                                                     % The names of the individual .wav files should match the audio_file field in the EEG data structures.
+        highpass_cutoff (1,1) double {mustBeReal} = 2   % highpass_cutoff, defaults to 2 Hz 
+        lowpass_cutoff  (1,1) double {mustBeReal} = 35  % lowpass_cutoff, defaults to 35 Hz
         options.StartTimeOffset (1,1) double {mustBeReal} = NaN % optional offset to the start time of the analysis epoch, in seconds, relative to the original EEG onset.  
                                                                 % If NaN, use the original EEG onset as the start of the analysis epoch.
         options.EpochDuration (1,1) double {mustBeReal} = NaN   % optional epoch duration, in seconds-- if NaN, use the full length of the EEG epochs as the analysis epoch duration.
     end
-    
-    % We create two equal-length lists of eeg data files and speech datafiles
-    % Iterate through the two lists, 
-    %    analyzing the entrainment between one eeg data set and one speech dataset.     
-    % arguments
-    % 
-    % EEG data structure will have this format:
+    %--------------------------------------------------------------------------------------%
+    % EEG data structure will have a format APPROXIMATELY LIKE THIS:
     % EEG_struct = 
     %   struct with fields:
     %      OnsetLatency: [39×1 double]
@@ -41,10 +29,11 @@ function [] = run_IPC_dataset_list(eeg_data_filenames, speech_files_path, highpa
     %           Subj_id: 'SE0001'           
     %      Num_channels: 64
     %     exposure_type: "e"
-    
+    %--------------------------------------------------------------------------------------%
+
     %------------------------------------------------------------------------------------------------------%    
     % Iterate through all the EEG data files.
-    % And run Apply2Dataset_InstaPhaseCoherence() for each one, using the corresponding speech data. 
+    % Run Apply2Dataset_InstaPhaseCoherence() for each one, using the corresponding speech data. 
     % For each EEG data file, we load a data structure called EEG_struct
     % and then we load the corresponding speech data from a .wav file
     % collect the results into an array of structures called ???.
@@ -83,32 +72,30 @@ function [] = run_IPC_dataset_list(eeg_data_filenames, speech_files_path, highpa
         Speech_RawData = struct;                            % Initialize structure
         Speech_RawData.Amplitudes = speech_amplitudes;      % Save the speech amplitudes
         Speech_RawData.Fs = fs_speech;                      % Save the speech sampling rate (44.1 khz for .wav files)
-        % Create a filename for saving analysis results to the same folder as the EEG data.
-        % The name will be unique to the EEG data filename (e.g., include the subject ID and condition) 
         
-        % Each call to Apply2Datset_CrossSpectralDensity returns a structure with the results for one EEG data set and one speech data set.
-        % we collect those structures in an array called ipc_results_structures.
-        
-        % The call to Apply2Dataset_CrossSpectralDensity() will be 
-        % contingent on the optional arguments for epoching.  If the user specifies those arguments, we pass them to the function, and if they don't, we call the function without those arguments, and it will use the default values (which are to use the full length of the EEG epochs as the analysis epoch duration, and to use the original EEG onset as the start of the analysis epoch).
-        % 
-        % The output file will include the name of the folder we created for the results of this analysis, 
-        % and the file name will include the the EEG filename (which includes the subject ID and condition)
-        % 
-        % The filename will NOT include the filter parameters and epoching parameters.  We'll try to keep the filenames simpler. 
-        % We will include the meta data in the results structure itself, so that we can keep track of which results correspond to which analysis parameters without having to parse the filename.
-        % We will also write a sidecar file that contains the name of the EEG data file that this results file corresponds to, and any other relevant information about the analysis parameters (e.g., filter parameters, epoching parameters, etc.) that we want to keep track of.
+        % --------------------------------------------------------------------------------------%
+        % Each call to Apply2Datset_InstaPhaseCoherence returns a structure, which we'll call Results_IPC_Analysis.
+        % Results_IPC_Analysis will contain the results of the Instantaneous Phase Coherence analysis for one subject and one condition.
+        % The Results_IPC_Analysis data structure will include fields for meta data about the analysis parameters,
+        % including filter parameters, epoching parameters, etc., so that we can keep track of the analysis parameters that were used for each results file.
+        % --------------------------------------------------------------------------------------%
+        % We create a filename, ipc_output_file for saving the analysis results.  
+        % The filename will include the EEG data structure filename, which records the subject ID and some condition information. 
+        % --------------------------------------------------------------------------------------%
         ipc_output_file = sprintf("%s/IPC_results_%s.mat", output_dir, eeg_filestem);
+        % If the user specified optional values for the epoching parameters, we pass those to the function, and if they don't, we call the function without those arguments, 
+        % and it will default to the full length of the EEG epochs as the analysis epoch duration.    
         if isnan(options.StartTimeOffset) || isnan(options.EpochDuration)
-            Results_IPC_Analysis = Apply2Dataset_CrossSpectralDensity(EEG_struct, Speech_RawData, highpass_cutoff, lowpass_cutoff);
-        else    
-            Results_IPC_Analysis = Apply2Dataset_CrossSpectralDensity(EEG_struct, Speech_RawData, highpass_cutoff, lowpass_cutoff, StartTimeOffset=options.StartTimeOffset, EpochDuration=options.EpochDuration); 
+            Results_IPC_Analysis = Apply2Dataset_InstaPhaseCoherence(EEG_struct, Speech_RawData, highpass_cutoff, lowpass_cutoff);
+        else  
+            Results_IPC_Analysis = Apply2Dataset_InstaPhaseCoherence(EEG_struct, Speech_RawData, highpass_cutoff, lowpass_cutoff, StartTimeOffset=options.StartTimeOffset, EpochDuration=options.EpochDuration); 
         end
 
         % Write each individual subject's result structure, contents of the variable Results_IPC_Analysis, to a file.
         save(ipc_output_file, 'Results_IPC_Analysis');
-        % We write a sidecar file (.txt) that contains the name of the EEG data file that this results file corresponds to
-        % and other relevant information about the analysis parameters (e.g., filter parameters, epoching parameters, etc.) that we want to keep track of.
+        % We write a sidecar file to accompany each results file, 
+        % which records information about the analysis parameters (e.g., filter parameters, epoching parameters, etc.) that we want to keep track of.
+        % The sidecar file is in text format for easy viewing and sharing, and it has the same name as the results file but with "_metadata.txt" appended to the end of the filename.
         metadata_output_file_txt = sprintf("%s/IPC_results_Metadata_%s.txt", output_dir, eeg_filestem);  % Create a filename for the metadata text file, which has the same name as the results file but with "_metadata.txt" appended to the end of the filename.
         fid = fopen(metadata_output_file_txt, 'w');  % Open the metadata text file for writing
         fprintf(fid, 'Metadata for Instantaneous Phase Coherence analysis results file\nResults filename: %s\n', ipc_output_file);  % 
@@ -118,7 +105,5 @@ function [] = run_IPC_dataset_list(eeg_data_filenames, speech_files_path, highpa
         fprintf(fid, 'Epoch start time offset: %d seconds\n', options.StartTimeOffset);  % Write the epoch start time offset used for this analysis
         fprintf(fid, 'Epoch duration: %d seconds\n', options.EpochDuration);      % Write the epoch duration used for this analysis
         fclose(fid);  % Close the metadata text file
-
-        
     end
 end
