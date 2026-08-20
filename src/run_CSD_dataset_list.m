0306066ea34d5161e77993e041fb5ad47f43c314
alkimcogneuro <albert.kim@colorado.edu>
2026-08-19 18:45:42 -0600

    Update run_CSD_dataset_list.m
    computes the random order control analysis using derange()

--- src/run_CSD_dataset_list.m
+++ src/run_CSD_dataset_list.m
@@ -1,5 +1,4 @@
 
-
 function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_files_path, analysis_ID_label, highpass_cutoff, lowpass_cutoff, options)
     % This script runs the cross spectral density analysis for all data files in a list of filenames, using the specified filter parameters.
     % For each data file, it runs Apply2Dataset_CrossSpectralDensity() to analyze the entrainment between the EEG data and the corresponding speech data,
@@ -9,16 +8,18 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
         eeg_data_filenames (1,:) cell {mustBeText}   % cell array of strings; each string is a filepath to an EEG data file.
         speech_files_path (1,1) string {mustBeText}  % string, path to the folder containing the speech .wav files.  
         % The names of the individual .wav files should match the audio_file field in the EEG data structures.
-        analysis_ID_label (1,1) string {mustBeText}  % string label for this analysis, which will be used in the output filenames and figure titles.  This is optional, and defaults to "main_analysis" if not specified.
+        analysis_ID_label (1,1) string {mustBeText}  % string label for this analysis, which will be used in the Results folder name.  
         highpass_cutoff (1,1) double {mustBeReal} = 2   % highpass_cutoff, defaults to 2 Hz 
         lowpass_cutoff  (1,1) double {mustBeReal} = 35  % lowpass_cutoff, defaults to 35 Hz
         options.StartTimeOffset (1,1) double {mustBeReal} = NaN % optional offset to the start time of the analysis epoch, in seconds, relative to the original EEG onset.  
         % If NaN, use the original EEG onset as the start of the analysis epoch.
         options.EpochDuration (1,1) double {mustBeReal} = NaN   % optional epoch duration, in seconds-- if NaN, use the full length of the EEG epochs as the analysis epoch duration.
-        options.RandomizeOnsets (1,1) logical = false  % optional flag for whether to randomize the onset times of the speech epochs relative to the EEG data.  
-        % If true, the speech onset times will be randomly changed.  
-        % This can be used as a control analysis to test whether any observed entrainment effects are specific to the original alignment of the speech and EEG data, 
-        % or if they could be explained by non-specific temporal correlations.
+        options.RandomizeOnsets (1,1) logical = false   % optional flag indicates whether to randomize the onset times of the speech epochs relative to the EEG data.  
+                                                        % If true, the speech onset times will be randomly changed.  
+                                                        % This can be used as a control analysis to test whether any observed entrainment effects 
+                                                        % are specific to the original alignment of the speech and EEG data, 
+                                                        % or if they could be explained by non-specific temporal correlations.
+                                                        % Default value is false.  
     end
     fprintf('Running run_CSD_dataset_list() for %d EEG data files, with highpass cutoff %.2f Hz and lowpass cutoff %.2f Hz\n', length(eeg_data_filenames), highpass_cutoff, lowpass_cutoff);
     %--------------------------------------------------------------------------------------%
@@ -33,11 +34,10 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
     %                Fs: 1000
     %              Data: [64×3000×39 single]
     %          Chanlocs: [1×64 struct]
-    %             Block: 'P1'               % should change this to "Condition" in the future, to be more general and less tied to our specific experiment.
-    %        audio_file: 'Velveteen_pseudo.wav'
+    %         Condition: 'Condition1_Task2'      % .
+    %        audio_file: 'MyStory2.wav'
     %           Subj_id: 'SE0001'           
     %      Num_channels: 64
-    %     exposure_type: "e"
     %--------------------------------------------------------------------------------------%
     % Create a folder for the results of this analysis, 
     % which will hold the results files for analysis of all files in the list eeg_data_filenames
@@ -49,7 +49,6 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
         mkdir(output_dir);  % if the output folder does not exist, create it
     end
     fprintf('   Output directory for CSD results: %s\n', output_dir);  % print the output directory to the command window
-
     %------------------------------------------------------------------------------------------------------%    
     % Iterate through all the EEG data files.
     % Run Apply2Dataset_CrossSpectralDensity() for each one, using the corresponding speech data. 
@@ -59,9 +58,9 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
     % collect the results into an array of structures called csd_results_structures.
     %------------------------------------------------------------------------------------------------------%    
     result_file_list = [];
-    All_Results_CSD_Analysis = struct;  % Initialize a structure to hold the results for all subjects and conditions, which will be saved to a single .mat file at the end of the function.
+    All_Results_CSD_Analysis = struct;  % Initialize a structure to hold the results for all subjects and conditions, which will be saved to a .mat file at the end of the function.
     for idx = 1:length(eeg_data_filenames)
-        fprintf ('  run_CSD_dataset_list() \n\n   Processing eeg data file %d:  %s\n', idx, eeg_data_filenames{idx});
+        fprintf ('  run_CSD_dataset_list() \n\n  Processing EEG data file %d:  %s\n', idx, eeg_data_filenames{idx});
         eeg_data_file = eeg_data_filenames{idx};    % grab a single eeg data file name
         [eeg_filepath, eeg_filestem, eeg_fileext] = fileparts(eeg_data_file);   % segment the file path into its components
         load(eeg_data_file);                        % Load EEG data structure from one file 
@@ -70,11 +69,13 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
         % We use that to load the correct speech data for this subject and condition.
         fprintf('Current EEG Data corresponds to AudioFile: %s\n', EEG_struct.audio_file);
         % Read speech from a file and package it just a bit.
+        % We combine the function argument speech_files_path and the file name in EEG_struct.audio_file, 
+        % and then we read the speech data from that file.
         speech_full_filepath = fullfile(speech_files_path, EEG_struct.audio_file);  % Create full path to speech file
         fprintf('Audio file full path: %s\n', speech_full_filepath);
-        [speech_amplitudes, fs_speech] = audioread(speech_full_filepath);          % Reaad speech raw data from a .wav file
+        [speech_amplitudes, fs_speech] = audioread(speech_full_filepath);          % Read speech raw data from a .wav file
         Speech_RawData = struct;                            % Initialize structure
-        Speech_RawData.Amplitudes = speech_amplitudes;      % Save the speech amplitudes
+        Speech_RawData.Amplitudes = speech_amplitudes;      % Save the speech amplitudes, the full length of the speech file.  
         Speech_RawData.Fs = fs_speech;                      % Save the speech sampling rate (44.1 khz for .wav files)
         % --------------------------------------------------------------------------------------%
         % Each call to Apply2Datset_CrossSpectralDensity returns a structure, which we'll call Results_CSD_Analysis.
@@ -93,14 +94,17 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
 
         Results_CSD_Analysis = struct;  % Initialize the results structure for this subject and condition, which will be filled with the output of Apply2Dataset_CrossSpectralDensity() and then saved to a file.
         if isnan(options.StartTimeOffset) || isnan(options.EpochDuration)
+            % apply the cross spectral density analysis to a single EEG dataset and speech data, 
+            % using the specified filter parameters, and default epoching parameters (full length of EEG epochs)
             Results_CSD_Analysis = Apply2Dataset_CrossSpectralDensity(EEG_struct, Speech_RawData, highpass_cutoff, lowpass_cutoff, RandomizeOnsets=options.RandomizeOnsets);
         else    
+            % if the user has supplied StartTimeOffet and EpochDuration, we pass those to the function.
             Results_CSD_Analysis = Apply2Dataset_CrossSpectralDensity(EEG_struct, Speech_RawData, highpass_cutoff, lowpass_cutoff, ...
-            StartTimeOffset=options.StartTimeOffset, EpochDuration=options.EpochDuration, RandomizeOnsets=options.RandomizeOnsets); 
+                        StartTimeOffset=options.StartTimeOffset, EpochDuration=options.EpochDuration, RandomizeOnsets=options.RandomizeOnsets); 
         end
         % Write each individual subject's result structure, contents of the variable Results_CSD_Analysis, to a file.
         save(csd_output_file, 'Results_CSD_Analysis');
-        % In addition to saving the individual subjects analyses to seprarate files, 
+        % In addition to saving the individual subjects analyses to separate files, 
         % we will also collect all the results into a structure array called All_Results_CSD_Analysis, 
         % where each element of the array corresponds to one subject and condition, 
         % and has a field called Results_CSD_Analysis which contains the results for that subject and condition.
@@ -111,7 +115,7 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
     
     all_results_file = fullfile(output_dir, "All_Results_CSD_Analysis.mat");  % Create a filename for the All_Results_CSD_Analysis structure, which will be saved to a .mat file in the output directory.
     save(all_results_file, 'All_Results_CSD_Analysis');    % Save the All_Results_CSD_Analysis structure to a .mat file in the output directory.
-            
+    %---------------------------------------------------------------------------------------------------------%            
     % Save meta data for this run.
     % this will include:
     %   -- the list of results files
@@ -121,6 +125,8 @@ function [result_file_list] = run_CSD_dataset_list(eeg_data_filenames, speech_fi
     %   -- the date and time when the analysis was run, which will be useful for keeping track of different runs of the analysis,
     %  -- the name of the analysis script and function used for this analysis, which will be useful for keeping track of the code that was used to generate the results,
     % and any other parameters that we want to keep track of for this analysis.  
+    %---------------------------------------------------------------------------------------------------------%            
+
     metadata_output_file = fullfile(output_dir, "CSD_results_Metadata.txt");  % Create a filename for the metadata text file for this run
     fid = fopen(metadata_output_file, 'w');  % Open the metadata text file for writing
     fprintf(fid, 'Metadata for Cross Spectral Density (and Magnitude Squared Coherence) Analysis\n\n');  % 
